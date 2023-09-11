1ffaabb07ea88967f5c964ceb7e2c50af668af5a
yiseul_notebook <kimdew402@gmail.com>
2023-09-11 18:21:16 +0900

    중요한 데이터는 변수말고 state에 담습니다.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  
+  // 재 랜더링이 자동으로 되기 때문에 useState를 사용한다. 
+  let [글제목, b] = useState('남자 코트 추천');
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h4>블로그</h4>  
-      </div>    
-      <h4>{ post }</h4>  
+        <h4>ReactBlog</h4>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
     </div>
   );
 }
