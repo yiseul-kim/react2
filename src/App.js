abd53ea19a318c4cf6998160d748c02c47204a6f
yiseul_notebook <kimdew402@gmail.com>
2023-09-12 19:30:19 +0900

    array, object state 변경하는 법

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   let post = '강남 우동 맛집';
 
   // 재 랜더링이 자동으로 되기 때문에 useState를 사용한다. 
-  let [글제목, b] = useState(['남자 코트 추천', '강남 우동맛집', '파이선독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이선독학']);
 
   let [따봉, 따봉변경] = useState(0);
 
@@ -21,11 +21,17 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
 
+      <button onClick={() => {
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천';
+        글제목변경(copy); 
+      }}>글수정</button>
+
       <div className="list">
-        
-        <h4><button onClick={ ()=>{ b(['여자코트추천', '강남 우동맛집', '파이선독학']) } }>제목변경</button><br/>
-        {글제목[0]} <span onClick={ ()=>{ 따봉변경(따봉+1) }}>👍</span>
-         {따봉} </h4>
+
+        <h4>
+          {글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>👍</span>
+          {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
