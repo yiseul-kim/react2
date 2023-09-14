43cbbffda8eed556944c8b2a5c84bd7ffb7a16d5
yiseul_notebook <kimdew402@gmail.com>
2023-09-14 18:13:11 +0900

    props를 응용한 상세페이지 만들기

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
   // 재 랜더링이 자동으로 되기 때문에 useState를 사용한다. 
   let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이선독학']);
 
-  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [따봉, 따봉변경] = useState(0);
   let [modal, setModal] = useState(false);
+  let [title, settitle] = useState(0);
 
   [1, 2, 3].map(function (a) {
     return '12314123'
@@ -51,13 +52,9 @@ function App() {
         글제목.map(function (a, i) {
           return (
             <div className="list" key={i}>
-              <h4 onClick={() => { setModal(!modal) }}>{글제목[i] }
-                <span onClick={() => { 
-                  let copy = [...따봉];
-                  copy[i] = 따봉[i]+1;
-                  따봉변경(copy);
-                }}>👍</span>
-                {따봉[i]} </h4>
+              <h4 onClick={ () => { setModal(true); settitle(i) }}>
+                {글제목[i]} <span onClick={() => { 따봉변경(따봉+1) }} >
+                  👍</span> {따봉} </h4>
               <p>2월 17일 발행</p>
             </div>
           )
@@ -67,7 +64,7 @@ function App() {
 
       {
         // 삼항연산자 : 조건식 ? 참일때 실행할 코드 : 거짓일 때 실행할 코드 
-        modal == true ? <Modal  글제목작명={글제목} 글제목변경={글제목변경}/> : null
+        modal == true ? <Modal 글제목작명={글제목} title={title} /> : null
       }
 
     </div>
@@ -81,18 +78,19 @@ function Modal(props) {
     // color="orange" 를 Modal 뒷부분에 넣으면 사용 가능
     // <div className="modal" style={{background : props.color}}>
     <div className="modal">
-      <h4>{props.글제목작명[0]}</h4>
+      <h4>{ props.글제목작명[props.title ]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
 
-      <div><button onClick={ ()=>{
-        let copy = [...props.글제목작명];
-        copy[0] = '여자코트 추천';
-        props.글제목변경(copy);
-      }}>글수정</button></div>
-      
     </div>
   )
 }
+
+
+// 동적인 ui 만들기 3step
+// 1. html css로 미리 디자인
+// 2. 현재 ui상태를 state에 저장해 둠
+// 3. state에 따라 ui가 어떻게 보일지 작성
 
 export default App;
